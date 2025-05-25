9878720182f4fb7350a917d984f08f6aa10d6442
ken travis <kentravis37@gmail.com>
2025-05-25 16:17:24 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import CartButton from "../components/cartButton";
 import { CartProvider } from "../context/CartContext";
@@ -7,19 +7,22 @@ import ProductDetails from "./pages/ProductDetails";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CartPage from "./pages/Cart";
+// import CartPage from "./pages/Cart";
 
 function App() {
   return (
     <>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-        </Routes>
-        <CartButton />
-         <ToastContainer position="top-right" />
-      </CartProvider>
+      <HashRouter>
+        <CartProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+          </Routes>
+          <CartButton />
+          <ToastContainer position="top-right" />
+        </CartProvider>
+      </HashRouter>
     </>
 
   );
